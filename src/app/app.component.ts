e305274e1b404cbd047524a5fddb79097cb0ca10
Técnico de Informática 2021.1 <39127512021.1@senacrjedu.local>
2023-05-12 19:49:54 -0300

    Ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder,ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'censo';
+  title = 'formulario';
+  cadastro = this.formBuilder.group({
+    nome: '',
+    email: ''});
+
+  constructor( private formBuilder: FormBuilder){
+
+  }
+  
+  mensagem(){
+    window.alert("A mensagem");
+  }
+
+
+  onSubmit(){
+    let obj = this.cadastro.value;
+    console.log(this.cadastro.value);
+    
+    alert(obj.nome);
+  }
 }
